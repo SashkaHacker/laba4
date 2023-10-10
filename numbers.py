efc1027d43988a6495887ad04af3a861dea3a7a7
SashkaHacker <am942081@gmail.com>
2023-10-10 20:28:26 +0300

    update

--- numbers.py
+++
@@ -1,2 +0,0 @@
-first, second, third, fourth = [int(input()) for i in range(4)]
-print(round((first + second) / (third + fourth), 2))
--- user.py
+++
@@ -1,4 +0,0 @@
-name = input("What is your name?")
-age = input("How old are you?")
-place = input("Where are you live?")
-print(f"This is {name}\nIt is {age}\n(S)he live in {place}")
