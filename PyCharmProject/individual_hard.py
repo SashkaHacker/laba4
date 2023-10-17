1b363d8f8732d6d399db1dd854f990e82f5002d1
SashkaHacker <am942081@gmail.com>
2023-10-17 12:57:31 +0300

    Fix individual hard

--- PyCharmProject/individual_hard.py
+++ PyCharmProject/individual_hard.py
@@ -1,2 +1,4 @@
-a2, a1, b = int(input()), int(input()), int(input())
-print(a2 + (a1 + b) // 10, (a1 + b) % 10)
+# вариант 3
+a3, a2, a1, b2, b1 = input(), input(), input(), input(), input()
+integer = str(int(a3 + a2 + a1) + int(b2 + b1))
+print(integer[0], integer[1], integer[2])
